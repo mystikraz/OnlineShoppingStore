89e851eef189d2b5c61fa2b71a30123d363778bb
Raj Tandukar <Raj@ws09.lan.npn.com.np>
2016-09-23 16:31:02 +0545

    FIRST COMMIT

---
+++ OnlineShopingStore.Domain/Abstract/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using OnlineShopingStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShopingStore.Domain.Abstract
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> Products { get; }
+    }
+}
